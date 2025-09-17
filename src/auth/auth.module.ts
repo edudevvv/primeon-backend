9d9c179092af4b2027e59419367b6bde2ead0456
pgwllz <storee.fantasy@gmail.com>
2025-09-17 11:21:16 -0300

    auth module fixed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 
 import { AuthController } from "./auth.controller";
-import { PrismaService } from "@/common/prisma/prisma.service";
+import { PrismaService } from "../common/prisma/prisma.service";
 import { AuthRepository } from "./repository/auth.repository";
 import { AuthService } from "./service/auth.service";
 import { JwtStrategy } from "./strategy/jwt.strategy";
